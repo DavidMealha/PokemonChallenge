4d621359d1a890e18191f3e97792aedfb2cdecbe
DavidMealha <davidmealha@hotmail.com>
2018-06-22 18:35:42 +0100

    using intellij to streamline the development

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,14 +3,14 @@ package app;
 public class Main {
 
     public static void main(String[] args) {
-    	String movements = "";
-    
-    	try {
-    		movements = args[0];
-    	} catch (ArrayIndexOutOfBoundsException e) {
-			System.err.println("Missing argument!");
-			System.exit(0);
-    	}
+        String movements = "";
+
+        try {
+            movements = args[0];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println("Missing argument!");
+            System.exit(0);
+        }
 
         new PokemonApplication()
                 .Start(movements);
